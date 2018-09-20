bb87ec4932e2cb192836c12ab394eed7865cf1c8
beliefgp <ggjqzjgp103@qq.com>
2018-09-20 17:47:30 +0800

    typing: 完善 ts 声明

--- index.d.ts
+++ index.d.ts
@@ -1,22 +1,53 @@
-// 表明依赖egg，否则会覆盖egg的声明
-import 'egg';
+import { Application, Context, PlainObject } from 'egg';
 
-declare module 'egg' {
-
+namespace EggBizError {
   interface bizConfig {
     code?: string | number; 
     bizError?: boolean;
     log?: boolean;
-    sendClient?: any;
+    sendClient?: PlainObject;
     [prop: string]: any;
   }
 
-  interface Context {
-    throwBizError(code: string | number, error?: Error | string, bizParams?: bizConfig): never;
-    throwBizError(code: string | number, bizParams: bizConfig): never;
-    throwBizError(error: Error, bizParams?: bizConfig): never;
-    throwBizError(bizParams: bizConfig): never;
+  interface CustomError extends Error {
+    code: string;
+    bizError: true;
+    bizParams: PlainObject;
+    log: boolean;
+  }
 
-    responseBizError(error: Error, bizParams?: bizConfig): void;
+  interface Config {
+    code: string | number,
+    message: string,
+    status: number;
+    breakDefault: boolean;
+    errorPageUrl?: string | ((ctx: Context, error: CustomError) => any);
+    errors?: PlainObject;
+  }
+
+  class BizErrorHandler {
+    app: Application;
+    config: Config;
+    responseError(ctx: Context, error: CustomError): void;
+    getOutputInfo(config: Config): any;
+    accepts(ctx: Context): string;
+    html(ctx: Context, error: CustomError, config: Config): void;
+    json(ctx: Context, error: CustomError, config: Config): void;
+    jsonp(ctx: Context, error: CustomError, config: Config): void;
+  }
+}
+
+declare module 'egg' {
+  interface Application {
+    BizErrorHandler: typeof EggBizError.BizErrorHandler;
+  }
+
+  interface Context {
+    throwBizError(code: string | number, error?: Error | string, bizParams?: EggBizError.bizConfig): never;
+    throwBizError(code: string | number, bizParams: EggBizError.bizConfig): never;
+    throwBizError(error: Error, bizParams?: EggBizError.bizConfig): never;
+    throwBizError(bizParams: EggBizError.bizConfig): never;
+
+    responseBizError(error: Error, bizParams?: EggBizError.bizConfig): void;
   }
 }
